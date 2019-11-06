1bd0ed25036c298cb6ef0a1c5cdcd6ad0b472591
Kimberly <klindholm7@gmail.com>
2019-11-05 19:28:52 -0700

    life simulator public interface done except for update function

--- LifeSimulator.cpp
+++ LifeSimulator.cpp
@@ -5,8 +5,8 @@
 //You'll want to set these equal to the width/height of the console the program runs within.
 //TODO: if you are getting weird results maybe need to swap x and y we have y as the vector of vectors
 LifeSimulator::LifeSimulator(std::uint8_t sizeX, std::uint8_t sizeY) {
-	sizeX = sizeX;
-	sizeY = sizeY;
+	sizeX = sizeX+3;
+	sizeY = sizeY+3;
 	//fill up your world with dead cells
 	for (size_t i = 0; i < sizeY; i++)
 	{
@@ -14,14 +14,25 @@ LifeSimulator::LifeSimulator(std::uint8_t sizeX, std::uint8_t sizeY) {
 		{
 			cell tmp;
 			tmp.alive = false;
-			myPattern[i].push_back(tmp);
+			currentState[i].push_back(tmp);
 		}
 	}
 }
 
 //insertPattern - Adds the pattern to the world, with the upper left corner beginning at startXand startY.
 void LifeSimulator::insertPattern(const Pattern& pattern, std::uint8_t startX, std::uint8_t startY) {
-
+	for (size_t i = 0; i < pattern.getSizeY(); i++)
+	{
+		for (size_t j = 0; j < pattern.getSizeY(); j++)
+		{
+			//check that the cell is in range of the world
+			if (i + startY >= sizeY || j + startX >= sizeX) {
+				continue;
+			}
+			//if in bounds update state of cell to be same as corresponding pattern cell
+			currentState[i + startY][j + startX].alive = pattern.getCell(i, j);
+		}
+	}
 }
 
 //update - Performs a single step update of the world, following the four rules specified in the wiki article.
