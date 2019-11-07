17019ffd3c7870cf259e7ed757c3e8908a6073f4
Kimberly Lindholm <klindholm7@gmail.com>
2019-11-06 18:02:37 -0700

    works on g++

---
+++ build/CMakeFiles/ConwaysLife.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Documents/Conways-Game/LifeSimulator.cpp" "/home/pi/Documents/Conways-Game/build/CMakeFiles/ConwaysLife.dir/LifeSimulator.cpp.o"
+  "/home/pi/Documents/Conways-Game/PatternAcorn.cpp" "/home/pi/Documents/Conways-Game/build/CMakeFiles/ConwaysLife.dir/PatternAcorn.cpp.o"
+  "/home/pi/Documents/Conways-Game/PatternBlinker.cpp" "/home/pi/Documents/Conways-Game/build/CMakeFiles/ConwaysLife.dir/PatternBlinker.cpp.o"
+  "/home/pi/Documents/Conways-Game/PatternBlock.cpp" "/home/pi/Documents/Conways-Game/build/CMakeFiles/ConwaysLife.dir/PatternBlock.cpp.o"
+  "/home/pi/Documents/Conways-Game/PatternGlider.cpp" "/home/pi/Documents/Conways-Game/build/CMakeFiles/ConwaysLife.dir/PatternGlider.cpp.o"
+  "/home/pi/Documents/Conways-Game/PatternGosperGliderGun.cpp" "/home/pi/Documents/Conways-Game/build/CMakeFiles/ConwaysLife.dir/PatternGosperGliderGun.cpp.o"
+  "/home/pi/Documents/Conways-Game/RendererConsole.cpp" "/home/pi/Documents/Conways-Game/build/CMakeFiles/ConwaysLife.dir/RendererConsole.cpp.o"
+  "/home/pi/Documents/Conways-Game/main.cpp" "/home/pi/Documents/Conways-Game/build/CMakeFiles/ConwaysLife.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
