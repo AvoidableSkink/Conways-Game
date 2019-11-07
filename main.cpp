bce64b17fd28c81689cded81babdc0fa60a0c689
Kimberly <klindholm7@gmail.com>
2019-11-06 18:06:21 -0700

    this is what you turned in

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ int main() {
     // render the simulator
     RendererConsole console;
     console.render(myWorld);
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
     myWorld.update();
   }
 
